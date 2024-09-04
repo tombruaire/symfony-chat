8bcdc399dbb94794afbdaa296571791e91601526
Tom BRUAIRE <tombruaire@gmail.com>
2024-09-04 16:26:26 +0200

    Mise en place du traitement pour la modification du mot de passe via le Profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\ModifierMdpType;
 use App\Form\SupprimerCompteType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\ExpressionLanguage\Expression;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -15,7 +19,7 @@ class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
     #[IsGranted(new Expression('is_authenticated()'))]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $alert = "";
 
@@ -24,7 +28,17 @@ class ProfilController extends AbstractController
         $formModifierMdp->handleRequest($request);
 
         if ($formModifierMdp->isSubmitted() && $formModifierMdp->isValid()) {
-            //
+            // Modification du mot de passe dans la base de données
+            $email = $this->getUser()->getEmail();
+            $password = $formModifierMdp["password"]->getData();
+            $entityManager->getRepository(User::class)->resetPassword($email, $password, $userPasswordHasher);
+
+            // Déconnexion de l'utilisateur
+            $session = new Session();
+            $session->invalidate();
+
+            // Redirection vers la page de connexion
+            return $this->redirectToRoute('app_login');
         }
 
         /*** Suppression du compte ***/
