32c56fe19646570037336f031d51b700bcf51562
Tom BRUAIRE <tombruaire@gmail.com>
2024-09-04 16:08:39 +0200

    Création du formulaire de suppression de compte

---
+++ src/Form/SupprimerCompteType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class SupprimerCompteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => "Adresse email *",
+                'attr' => [
+                    'class' => 'form-control',
+                    'autocomplete' => "off",
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Veuillez saisir une adresse email !",
+                    ]),
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe *',
+                 'attr' => [
+                    'class' => 'form-control',
+                    'autocomplete' => 'off',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un mot de passe !',
+                    ]),
+                ],
+
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
