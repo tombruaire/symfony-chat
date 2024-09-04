5920ab97e653e1db9e40fbdee6ca366fb44632df
Tom BRUAIRE <tombruaire@gmail.com>
2024-09-04 12:12:11 +0200

    Création d'un formulaire pour l'email (Mot de passe oublié)

--- src/Controller/MdpOublierController.php
+++ src/Controller/MdpOublierController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\MdpOublierType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MdpOublierController extends AbstractController
 {
-    #[Route('/mdp-oublier', name: 'app_mdp_oublier')]
-    public function index(): Response
+    #[Route('/mdp-oublier', name: 'app_mdp_oublier', methods: ['GET','POST'])]
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(MdpOublierType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = $form["email"]->getData();
+            dd($email);
+        }
+
         return $this->render('mdp_oublier/index.html.twig', [
             'controller_name' => 'MdpOublierController',
+            'MdpOublierForm' => $form,
         ]);
     }
 }
