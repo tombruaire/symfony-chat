d9451c2a01831041b417b4c46ba7797510abcffa
tombr <tombruaire@gmail.com>
2024-09-05 17:10:28 +0200

    Ajout du traitement d'envoi de messages général

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,20 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\Messages;
+use App\Entity\User;
+use App\Form\ChatType;
+use App\Repository\MessagesRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\ExpressionLanguage\Expression;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class HomeController extends AbstractController
 {
+    /**
+     * @throws \DateMalformedStringException
+     */
     #[Route('/chat', name: 'app_home')]
     #[IsGranted(new Expression('is_authenticated()'))]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $message = new Messages();
+        $form = $this->createForm(ChatType::class, $message);
+        $form->handleRequest($request);
+
+        $userForm = $this->getUser();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message->setUserFrom($userForm);
+            $message->setUserTo(null);
+
+            // Création d'une nouvelle instance de DateTime
+            $dateEnvoi = new \DateTime();
+            // Ajout de 2 heures à la Date d'envoi
+            $dateEnvoi->modify('+2 hours');
+            // Assignement de la date modifiée à l'objet message
+            $message->setDateEnvoi($dateEnvoi);
+
+            $entityManager->persist($message);
+            $entityManager->flush();
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'users' => $entityManager->getRepository(User::class)->findAll(),
+            'ChatForm' => $form,
+            'messages' => $entityManager->getRepository(Messages::class)->finAllMessages(),
         ]);
     }
 }
