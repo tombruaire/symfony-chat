067829d73226455074e5a58dee6605533e73852b
Tom BRUAIRE <tombruaire@gmail.com>
2024-09-06 11:04:41 +0200

    Ajout du traitement pour l'affichage des demandes d'amis de l'utilisateur

--- src/Controller/AmisController.php
+++ src/Controller/AmisController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Amis;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,28 @@ use Symfony\Component\Routing\Attribute\Route;
 class AmisController extends AbstractController
 {
     #[Route('/amis', name: 'app_amis')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $demandes = $entityManager->getRepository(Amis::class)->findDemandesCible($this->getUser()->getId());
+
+        $userId = $this->getUser()->getId();
+
+        $cibleId = "";
+        foreach ($demandes as $demande) {
+            if ($demande["cible"] == $userId) {
+                $cibleId = $demande["cible"];
+            }
+            break;
+        }
+
+        $nbDemande = $entityManager->getRepository(Amis::class)->countDemandeAmi($this->getUser()->getId());
+
         return $this->render('amis/index.html.twig', [
             'controller_name' => 'AmisController',
+            'demandes' => $demandes,
+            'userId' => $userId,
+            'cibleId' => $cibleId,
+            'nbDemande' => $nbDemande,
         ]);
     }
 }
